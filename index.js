adacc87b48362b95ee47300a7b2a180604ad305f
Dmytro Oliinyk <oliynykdmitry83@gmail.com>
2024-01-23 09:57:40 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const weatherForm = document.querySelector(".weatherForm");
+const cityInput = document.querySelector(".sityInput");
+const card = document.querySelector(".card");
+const apiKey = "72f09b6a6091fbf62b032191154122f4"; 
+
+weatherForm.addEventListener("submit", async event => {
+
+});
+async function getWeatherData(city){
+
+}
+function displayWeatherData(data){
+
+}
+function displayWeatherEmoji(weatherId){
+
+}
+function displayError(message){
+
+}
